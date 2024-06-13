64d316fcfb5873335f621bc62d3718e235c9fdcc
Python Legion <it.s90x10@gmail.com>
2024-06-13 04:04:37 +0200

    Update config.py

--- config_data/config.py
+++ config_data/config.py
@@ -1,13 +1,23 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
+
+
 if not find_dotenv():
     exit("Переменные окружения не загружены т.к отсутствует файл .env")
 else:
     load_dotenv()
 
+
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 RAPID_API_KEY = os.getenv("RAPID_API_KEY")
+TEXT_SAVE_PATH = os.getenv("TEXT_SAVE_PATH", "texts")
+PHOTO_SAVE_PATH = os.getenv("PHOTO_SAVE_PATH", "photos")
+VIDEO_SAVE_PATH = os.getenv("VIDEO_SAVE_PATH", "videos")
+AUDIO_SAVE_PATH = os.getenv("AUDIO_SAVE_PATH", "audios")
+
+#RAPID_API_KEY = os.getenv("RAPID_API_KEY")
 DEFAULT_COMMANDS = (
     ("start", "Запустить бота"),
     ("help", "Вывести справку")
