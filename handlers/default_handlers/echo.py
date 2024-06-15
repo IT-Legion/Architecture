623a0ce2082f2ed73fb53030eed11556a65fcbb5
Python Legion <it.s90x10@gmail.com>
2024-06-15 08:52:43 +0200

    Update echo.py

--- handlers/default_handlers/echo.py
+++ handlers/default_handlers/echo.py
@@ -5,6 +5,7 @@ from loader import bot
 
 # Эхо хендлер, куда летят текстовые сообщения без указанного состояния
 @bot.message_handler(state=None)
+@log_usage
 def bot_echo(message: Message):
     bot.reply_to(
         message, "Эхо без состояния или фильтра.\n" f"Сообщение: {message.text}"
